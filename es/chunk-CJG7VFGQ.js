63c0a20ceb91b1dba5039a140b7cec36a95ab801
Félix García Sánchez <eudaimov@gmail.com>
2025-07-02 11:31:36 +0200

    Auto-generated commit

---
+++ es/chunk-CJG7VFGQ.js
@@ -0,0 +1,2 @@
+import{lb as a}from"./chunk-IWL4O7GL.js";export{a as internetRoute};
+/**i18n:8b0e000f8760c8e2dae7757b407ce8c973de700f97aa7939afa7e448303ce8fb*/
--- es/chunk-PVPD7FGO.js
+++
@@ -1,2 +0,0 @@
-import{lb as a}from"./chunk-IG6MTJDZ.js";export{a as internetRoute};
-/**i18n:10aceb6666b48eff6c48493bdf78805c6d203cbd80e126466e591c09a2a2d7f0*/
